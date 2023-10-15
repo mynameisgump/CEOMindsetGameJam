3d90aa860e3353c9a29b9706705a8439caf65eb3
Ethan Crann <ecrann@protonmail.com>
2023-10-15 10:54:39 -0230

    Getting Somewhere

--- src/ThreeApp.tsx
+++ src/ThreeApp.tsx
@@ -1,66 +1,64 @@
 import React, { useRef, useState, useMemo } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { OrbitControls, Gltf } from '@react-three/drei'
-import { CollideEvent, Physics, useBox, usePlane, Debug, useSphere } from '@react-three/cannon'
-import { Instances, Stats} from '@react-three/drei'
+import { Instances, Stats, Environment} from '@react-three/drei'
+import { Physics, RigidBody } from '@react-three/rapier'
+import { Box } from '@react-three/drei'
 
 
-
-
-const Plane = () => {
-  const [ref] = usePlane(() => ({ rotation: [-Math.PI / 2, 0, 0] }))
+const Floor = () => {
+  console.log("floor test")
   return (
-    <mesh name="Plane" receiveShadow ref={ref}>
-      <planeGeometry />
-      <meshStandardMaterial color="#f0f0f0" />
-    </mesh>
-  )
-}
+    <RigidBody type="fixed" colliders="cuboid" name="floor">
+      <Box
+        position={[0, -12.55 - 5, 0]}
+        scale={[200, 10, 200]}
+        rotation={[0, 0, 0]}
+        receiveShadow
+      >
+        <meshStandardMaterial color={"red"}/>
+      </Box>
+    </RigidBody>
+  );
+};
 
-type CubeProps = {
-  position: [x: number, y: number, z: number]
-}
-const Cube = ({position}: CubeProps) => {
 
-  const collisionEvent = (e: CollideEvent) => {
-    console.log("collision");
-    console.log(e);
-  }
+// const Manson = (props: GroupProps) => {
+//   const { nodes } = useGLTF(
+//     new URL("../../models/objects.glb", import.meta.url).toString()
+//   ) as unknown as {
+//     nodes: {
+//       pear: Mesh;
+//     };
+//   };
 
-  const [ref] = useSphere(() => ({ mass: 1, onCollide: collisionEvent, position: position }))
+//   return (
+//     <group {...props} scale={1}>
+//       <RigidBody position={[0, 2, 0]} colliders="hull">
+//         <Clone object={nodes.pear} castShadow receiveShadow />
+//       </RigidBody>
+//     </group>
+//   );
+// };
+
+const Meat = () => {
   return (
-    <Gltf src="/public/MansonFish.glb" ref={ref}></Gltf>
-    // <mesh name="Cube" castShadow ref={ref}>
-      
-    //   <boxGeometry />
-    //   <meshStandardMaterial color="orange" />
-    // </mesh>
-  )
-}
-
-const Sphere = ({position}: CubeProps) => {
-
-  const collisionEvent = (e: CollideEvent) => {
-    console.log("collision");
-    console.log(e);
-  }
-
-  const [ref] = useSphere(() => ({ mass: 1, onCollide: collisionEvent, position: position }))
-  return (
-    <mesh ref={ref}>
-      <sphereGeometry  args={[1, 32, 32]} />
-      <meshBasicMaterial color="orange" />
-    </mesh>
-
+    <RigidBody>
+      <mesh position={[0,5,0]} castShadow={true}>
+        <sphereGeometry args={[1, 32, 32]} />
+        <meshStandardMaterial color="hotpink" />
+      </mesh>
+    </RigidBody>
   )
 }
 
 const Grinder = () => {
-  // const [ref] = useBox(() => ({ mass: 1, position: [0, 0, 0] }))
   return (
-    <Gltf src="/public/Grinder_Sep.glb" castShadow >
+    <RigidBody colliders={"hull"} type={"fixed"}>
+      <Gltf src="/public/Grinder_Sep.glb" castShadow >
+      </Gltf>
+    </RigidBody>
 
-    </Gltf>
   )
 }
 
@@ -68,19 +66,31 @@ const Grinder = () => {
 const ThreeApp = () => {
  return (
     <div style={{width: "100vw", height: "100vh"}}>
-        <Canvas>
+        <Canvas shadows dpr={1}>
             <Stats showPanel={0} className="stats"  />
-            <ambientLight />
+            {/* <ambientLight /> */}
             <pointLight position={[10, 10, 10]} />
 
             <OrbitControls />
-            <Physics>
-              <Debug scale={1.1} color="black">
-              <Sphere position={[0, 30, 0]} />
-              <Plane></Plane>
-              </Debug>
+
+            <directionalLight
+                castShadow
+                position={[10, 10, 10]}
+                shadow-camera-bottom={-40}
+                shadow-camera-top={40}
+                shadow-camera-left={-40}
+                shadow-camera-right={40}
+                shadow-mapSize-width={1024}
+                shadow-bias={-0.0001}
+              />
+            <Environment preset="apartment" />
+
+            <Physics debug gravity={[0, -1, 0]} >
+              <Meat></Meat>
+              <Floor></Floor>
               <Grinder></Grinder>
             </Physics>
+            
         </Canvas>
     </div>
  )
