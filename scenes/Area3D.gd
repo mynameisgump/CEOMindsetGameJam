e67f7187ad48391f90339775517466b9a27bbb56
Ethan Crann <ecrann@protonmail.com>
2023-10-15 21:44:07 -0230

    Have destroy working with 1 sphere

---
+++ scenes/Area3D.gd
@@ -0,0 +1,20 @@
+extends Area3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+
+func _on_body_entered(body):
+	print(body);
+	if (body.is_in_group("Meat")):
+		body.destroy()
+	
+	pass # Replace with function body.
