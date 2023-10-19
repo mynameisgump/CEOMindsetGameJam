6773a12dbe78c8747df4b5d81b5961d311f4cff7
Ethan Crann <ecrann@protonmail.com>
2023-10-19 19:34:15 -0230

    Sky meat

--- scripts/main.gd
+++ scripts/main.gd
@@ -10,17 +10,18 @@ var MeatSphere = preload("res://scenes/meat_sphere.tscn");
 
 const totalMeats = 1000;
 var money = 0;
+const max_meat = 1000;
 
 var meat_to_spawn = 0;
-func add_meat_spheres(num: int):
-	for i in range(1,num,1):
-		var x = randf_range(-5,5);
-		var z = randf_range(-5,5);
-		var y = randf_range(20,60);
-		var meat_sphere = MeatSphere.instantiate();
-		meat.add_child(meat_sphere);
-		meat_sphere.connect("destroyed",self._on_meat_destroyed)
-		meat_sphere.set_position(Vector3(x,y,z));
+		
+func add_meat_sphere():
+	var x = randf_range(-96,96);
+	var z = randf_range(-96,96);
+	var y = randf_range(20,60);
+	var meat_sphere = MeatSphere.instantiate();
+	meat.add_child(meat_sphere);
+	# meat_sphere.connect("destroyed",self._on_meat_destroyed)
+	meat_sphere.set_position(Vector3(x,y,z));
 
 
 func _ready():
@@ -34,16 +35,20 @@ func _process(delta):
 		grinding_area.resume_grinding();
 		vat.dump()
 		money += 100
-	print(meat_to_spawn)
-	if meat_to_spawn > 0:
-		if meat_spawn_timer.is_stopped():
-			var x = randf_range(-2,2);
-			var meat_sphere = MeatSphere.instantiate();
-			meat.add_child(meat_sphere);
-			meat_sphere.set_position(meat_sphere_spawn.global_position);
-			meat_sphere.apply_central_impulse(Vector3(x,0,10))
-			meat_to_spawn -= 1
-			meat_spawn_timer.start()
+	
+	var total_meat = meat.get_child_count();
+	print(total_meat)
+	#if meat_to_spawn > 0:
+	if meat_spawn_timer.is_stopped() && total_meat < max_meat:
+		add_meat_sphere();
+		meat_spawn_timer.start()
+#			var x = randf_range(-2,2);
+#			var meat_sphere = MeatSphere.instantiate();
+#			meat.add_child(meat_sphere);
+#			meat_sphere.set_position(meat_sphere_spawn.global_position);
+#			meat_sphere.apply_central_impulse(Vector3(x,0,10))
+#			meat_to_spawn -= 1
+#			meat_spawn_timer.start()
 
 	
 
